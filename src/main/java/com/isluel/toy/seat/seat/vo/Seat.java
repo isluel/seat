eb46d5f839b8b8d302f9686d22a5c28d74bcd666
isluel <isluel@nate.com>
2025-04-02 22:01:07 +0900

    modify 주석 오탈자 수정

--- src/main/java/com/isluel/toy/seat/seat/vo/Seat.java
+++ src/main/java/com/isluel/toy/seat/seat/vo/Seat.java
@@ -12,7 +12,7 @@ public class Seat {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "thread_id", nullable = false)
+    @Column(name = "theater_id", nullable = false)
     private Integer theaterId;
 
     @Column(name = "name", nullable = false)
