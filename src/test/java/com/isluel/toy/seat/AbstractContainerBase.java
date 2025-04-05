c5705b2fcf89944e6cbc8e9b126fe6821f42bdf2
isluel <isluel@nate.com>
2025-04-05 23:46:21 +0900

    Test 환경의 Local Redis 설정 변경

--- src/test/java/com/isluel/toy/seat/AbstractContainerBase.java
+++ src/test/java/com/isluel/toy/seat/AbstractContainerBase.java
@@ -8,19 +8,19 @@ import org.testcontainers.containers.GenericContainer;
 @TestPropertySource(properties = "app.scheduling.enable=false")
 public abstract class AbstractContainerBase {
     // TestContainer 설정
-    static final String REDIS_IMAGE = "redis:6-alpine";
-    static final GenericContainer REDIS_CONTAINER;
-
-    static {
-        REDIS_CONTAINER = new GenericContainer(REDIS_IMAGE)
-                .withExposedPorts(6379)
-                .withReuse(true);
-        REDIS_CONTAINER.start();
-    }
-
-    @DynamicPropertySource
-    public static void overrideProps(DynamicPropertyRegistry registry){
-        registry.add("spring.redis.host", REDIS_CONTAINER::getHost);
-        registry.add("spring.redis.port", () -> ""+REDIS_CONTAINER.getMappedPort(6379));
-    }
+//    static final String REDIS_IMAGE = "redis:6-alpine";
+//    static final GenericContainer REDIS_CONTAINER;
+//
+//    static {
+//        REDIS_CONTAINER = new GenericContainer(REDIS_IMAGE)
+//                .withExposedPorts(6379)
+//                .withReuse(true);
+//        REDIS_CONTAINER.start();
+//    }
+//
+//    @DynamicPropertySource
+//    public static void overrideProps(DynamicPropertyRegistry registry){
+//        registry.add("spring.redis.host", REDIS_CONTAINER::getHost);
+//        registry.add("spring.redis.port", () -> ""+REDIS_CONTAINER.getMappedPort(6379));
+//    }
 }
